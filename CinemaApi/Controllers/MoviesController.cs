d3a02aa7e5383359ee6e897590406e057174ffdd
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-30 20:20:40 +0200

    Created CinemaApi project

---
+++ CinemaApi/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using CinemaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CinemaApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MoviesController : ControllerBase
+  {
+    private static List<Movie> movies = new List<Movie>
+    {
+      new Movie(){Id = 0, Name = "Mission Impossible", Language = "English"},
+      new Movie(){Id = 1, Name = "The Matrix", Language = "English"},
+    };
+
+    [HttpGet]
+    public IEnumerable<Movie> Get()
+    {
+      return movies;
+    }
+
+    [HttpPost]
+    public void Post([FromBody] Movie movie)
+    {
+      movies.Add(movie);
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id,[FromBody] Movie movie)
+    {
+      movies[id] = movie;
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      movies.RemoveAt(id);
+    }
+  }
+}
---
+++ CinemaApi/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaApi.Models
+{
+  public class Movie
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public  string Language { get; set; }
+  }
+}
