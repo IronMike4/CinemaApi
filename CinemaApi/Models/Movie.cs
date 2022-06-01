b986520fc18137c8c662b71f42c7e17e7ee2d5f0
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-06-01 20:50:19 +0200

    Implementing Model Validations

--- CinemaApi/Models/Movie.cs
+++ CinemaApi/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CinemaApi.Models
@@ -6,8 +7,11 @@ namespace CinemaApi.Models
   public class Movie
   {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Name cannot be null or empty")]
     public string Name { get; set; }
-    public  string Language { get; set; }
+    [Required(ErrorMessage = "Language cannot be null or empty")]
+    public string Language { get; set; }
+    [Required(ErrorMessage = "Rating cannot be null or empty")]
     public double Rating { get; set; }
     [NotMapped]
     public IFormFile Image { get; set; }
